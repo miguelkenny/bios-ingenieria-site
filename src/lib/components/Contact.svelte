07a854d159ab2adedb84b458743c4835316ade40
miguelkenny <miguelcastrokenny@gmail.com>
2024-11-13 12:11:58 -0300

    Actualizacion de Email de Contacto

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,18 +1,22 @@
 <section
   id="contact"
-  class="contact px-4 md:px-20 py-10 md:py-10 m-4 md:m-8 flex flex-col justify-between items-center gap-y-10  bg-Blue"
+  class="contact px-4 md:px-20 py-10 md:py-10 m-4 md:m-8 flex flex-col justify-between items-center gap-y-10 bg-Blue"
   style="border-radius: 30px;"
 >
   <div
-  id="contact"
-  class="contact relative flex flex-col md:flex-row justify-between items-start"
+    id="contact"
+    class="contact relative flex flex-col md:flex-row justify-between items-start"
   >
     <!-- Texto de Contacto -->
     <div class="w-full md:w-1/2 pr-0 md:pr-8">
-      <h2 class="font-clashg text-md md:text-lg text-left mt-4 uppercase font-bold text-LightBlue">
+      <h2
+        class="font-clashg text-md md:text-lg text-left mt-4 uppercase font-bold text-LightBlue"
+      >
         Contacto
       </h2>
-      <p class="font-clashg relative text-2xl md:text-5xl mt-2 font-medium text-left">
+      <p
+        class="font-clashg relative text-2xl md:text-5xl mt-2 font-medium text-left"
+      >
         Hablemos de seguridad y soluciones a medida para tu empresa
       </p>
       <p class="text-lg md:text-xl my-4 text-LightBlue">
@@ -25,16 +29,14 @@
     </div>
 
     <!-- Formulario de Contacto -->
-    <div class="w-full md:w-max bg-white p-2 md:p-8 py-10 mr-0 md:mr-20 rounded-3xl shadow-lg">
+    <div
+      class="w-full md:w-max bg-white p-2 md:p-8 py-10 mr-0 md:mr-20 rounded-3xl shadow-lg"
+    >
       <form class="py-2">
         <div class="w-full flex flex-col md:flex-row md:space-x-4">
           <!-- Contenedor para Nombre -->
           <div class="mb-4 w-full md:w-1/2">
-            <label
-              for="firstName"
-              class="font-clashg text-lg"
-              >Nombre</label
-            >
+            <label for="firstName" class="font-clashg text-lg">Nombre</label>
             <input
               type="text"
               id="firstName"
@@ -44,11 +46,7 @@
 
           <!-- Contenedor para Apellido -->
           <div class="mb-4 w-full md:w-1/2">
-            <label
-              for="lastName"
-              class="font-clashg text-lg"
-              >Apellido</label
-            >
+            <label for="lastName" class="font-clashg text-lg">Apellido</label>
             <input
               type="text"
               id="lastName"
@@ -57,11 +55,7 @@
           </div>
         </div>
         <div class="mb-4">
-          <label
-            for="email"
-            class="font-clashg text-lg mt-4"
-            >Email</label
-          >
+          <label for="email" class="font-clashg text-lg mt-4">Email</label>
           <input
             type="email"
             id="email"
@@ -70,11 +64,7 @@
         </div>
 
         <div class="mb-4 mt-4 md:mt-8">
-          <label
-            for="message"
-            class="font-clashg text-lg"
-            >Mensaje</label
-          >
+          <label for="message" class="font-clashg text-lg">Mensaje</label>
           <textarea
             id="message"
             class="w-full p-3 border-b-2 border-gray-200 rounded-lg"
@@ -94,7 +84,11 @@
   </div>
 
   <div class="flex flex-col gap-y-10 md:flex-row md:gap-x-20">
-    <p class="font-clashg relative text-xl mt-2 font-medium text-left">+54 0264 154369135</p>
-    <p class="font-clashg relative text-xl mt-2 font-medium text-left">biosing@hotmail.com.ar</p>
+    <p class="font-clashg relative text-xl mt-2 font-medium text-left">
+      +54 0264 154369135
+    </p>
+    <p class="font-clashg relative text-xl mt-2 font-medium text-left">
+      gustavo@biosing.com.ar
+    </p>
   </div>
 </section>
